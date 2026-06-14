ROZ-MOFUMOFU-ME/namp

commit 8d8a40167b89ceecc1579a6b87353c14c122976c

chore: Update zny-nomp package version to 1.2.7 and stratum-pool dependency URL
- Updated the zny-nomp package version to 1.2.7.
- Updated the URL for the stratum-pool dependency.

packages/portal/website/static/miner_stats.js
@@ -21,23 +21,18 @@ function getReadableHashRateString(hashrate){
 }
 
 function getReadableLuckTime(lucktime){
-	var luck = lucktime;
-	var timeUnits = [' Days', ' Hours', ' Minutes', ' Seconds'];
-	if (luck < 1) {
-		luck = luck * 24;
-		if (luck < 1) {
-			luck = luck * 60;
-			if (luck < 1) {
-				luck = luck * 60;
-				return luck.toFixed(2) + timeUnits[3];
-			} else {
-				return luck.toFixed(2) + timeUnits[2];
-			}
-		} else {
-			return luck.toFixed(2) + timeUnits[1];
-		}
-	}
-	return luck + timeUnits[0];
+        var luck = lucktime;
+        var timeUnits = [ ' Days', ' Hours', ' Minutes' ];
+        if (luck < 1) {
+                luck = luck * 24;
+                if (luck < 1) {
+                        luck = luck * 60;
+                        return luck.toFixed(2) + timeUnits[2];
+                } else {
+                        return luck.toFixed(2) + timeUnits[1];
+                }
+        }
+        return luck.toFixed(3) + timeUnits[0];
 }
 
 function timeOfDayFormat(timestamp){
@@ -169,11 +164,11 @@ function updateStats() {
 	// do some calculations
 	var luckDays = 0;
 	for (var w in statData.workers) { luckDays = luckDays + 1 / statData.workers[w].luckDays; }
-	luckDays = getReadableLuckTime(1 / luckDays);
+	luckDays = (1 / luckDays);
 	// update miner stats
 	$("#statsHashrate").text(getReadableHashRateString(totalHash));
 	$("#statsHashrateAvg").text(getReadableHashRateString(calculateAverageHashrate(null)));
-	$("#statsLuckDays").text(luckDays);
+	$("#statsLuckDays").text(getReadableLuckTime(luckDays));
 	$("#statsTotalImmature").text(totalImmature);
 	$("#statsTotalBal").text(totalBal);
 	$("#statsTotalPaid").text(totalPaid);
@@ -187,7 +182,7 @@ function updateWorkerStats() {
 		var saneWorkerName = getWorkerNameFromAddress(w);
 		$("#statsHashrate"+htmlSafeWorkerName).text(getReadableHashRateString(statData.workers[w].hashrate));
 		$("#statsHashrateAvg"+htmlSafeWorkerName).text(getReadableHashRateString(calculateAverageHashrate(saneWorkerName)));
-		$("#statsLuckDays"+htmlSafeWorkerName).text(statData.workers[w].luckDays);
+		$("#statsLuckDays"+htmlSafeWorkerName).text(getReadableLuckTime(statData.workers[w].luckDays));
 		$("#statsPaid"+htmlSafeWorkerName).text(statData.workers[w].paid);
 		$("#statsBalance"+htmlSafeWorkerName).text(statData.workers[w].balance);
 		$("#statsShares"+htmlSafeWorkerName).text(Math.round(statData.workers[w].currRoundShares * 100) / 100);
@@ -198,13 +193,13 @@ function addWorkerToDisplay(name, htmlSafeName, workerObj) {
 	var htmlToAdd = "";
 	htmlToAdd = '<div class="boxStats" id="boxStatsLeft" style="float:left; margin: 9px; min-width: 260px;"><div class="boxStatsList">';
 	htmlToAdd+='<div class="boxLowerHeader">'+name.replace(/[^\w\s]/gi, '')+'</div><div>';
-	htmlToAdd+='<div><i class="fa fa-tachometer"></i> <span id="statsHashrate'+htmlSafeName+'">'+getReadableHashRateString(workerObj.hashrate)+'</span> (Now)</div>';
-	htmlToAdd+='<div><i class="fa fa-tachometer"></i> <span id="statsHashrateAvg'+htmlSafeName+'">'+getReadableHashRateString(calculateAverageHashrate(name))+'</span> (Avg)</div>';
-	htmlToAdd+='<div><i class="fa fa-shield"></i> <small>Diff:</small> <span id="statsDiff'+htmlSafeName+'">'+workerObj.diff+'</span></div>';
-	htmlToAdd+='<div><i class="fa fa-cog"></i> <small>Shares:</small> <span id="statsShares'+htmlSafeName+'">'+(Math.round(workerObj.currRoundShares * 100) / 100)+'</span></div>';
-	htmlToAdd += '<div><i class="fa fa-gavel"></i> <small>Luck <span id="statsLuckDays' + htmlSafeName + '">' + getReadableLuckTime(workerObj.luckDays) +'</span></small></div>';
-	htmlToAdd+='<div><i class="fa fa-money"></i> <small>Bal: <span id="statsBalance'+htmlSafeName+'">'+workerObj.balance+'</span></small></div>';
-	htmlToAdd+='<div><i class="fa fa-money"></i> <small>Paid: <span id="statsPaid'+htmlSafeName+'">'+workerObj.paid+'</span></small></div>';
+	htmlToAdd+='<div><i class="fas fa-tachometer-alt fa-fw"></i> <span id="statsHashrate'+htmlSafeName+'">'+getReadableHashRateString(workerObj.hashrate)+'</span> (Now)</div>';
+	htmlToAdd+='<div><i class="fas fa-tachometer-alt fa-fw"></i> <span id="statsHashrateAvg'+htmlSafeName+'">'+getReadableHashRateString(calculateAverageHashrate(name))+'</span> (Avg)</div>';
+	htmlToAdd+='<div><i class="fas fa-unlock-alt fa-fw"></i> <small>Diff:</small> <span id="statsDiff'+htmlSafeName+'">'+workerObj.diff+'</span></div>';
+	htmlToAdd+='<div><i class="fas fa-cog fa-fw"></i> <small>Shares:</small> <span id="statsShares'+htmlSafeName+'">'+(Math.round(workerObj.currRoundShares * 100) / 100)+'</span></div>';
+	htmlToAdd+='<div><i class="fas fa-clock fa-fw"></i> <small>Luck:</small> <span id="statsLuckDays'+htmlSafeName+'">'+getReadableLuckTime(workerObj.luckDays)+'</span></div>';
+	htmlToAdd+='<div><i class="fas fa-money-bill-alt fa-fw"></i> <small>Bal:</small> <span id="statsBalance'+htmlSafeName+'">'+workerObj.balance+'</span></div>';
+	htmlToAdd+='<div><i class="fas fa-money-bill-alt fa-fw"></i> <small>Paid:</small> <span id="statsPaid'+htmlSafeName+'">'+workerObj.paid+'</span></div>';
 	htmlToAdd+='</div></div></div>';
 	$("#boxesWorkers").html($("#boxesWorkers").html()+htmlToAdd);
 }

packages/portal/libs/api.js
@@ -55,7 +55,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
 							portalStats.getTotalSharesByAddress(address, function(shares) {								
 								var totalHash = parseFloat(0.0);
 								var totalShares = shares;
-								var networkSols = 0;
+								var networkHash = 0;
 								for (var h in portalStats.statHistory) {
 									for(var pool in portalStats.statHistory[h].pools) {
 										for(var w in portalStats.statHistory[h].pools[pool].workers){
@@ -85,11 +85,11 @@ module.exports = function(logger, portalConfig, poolConfigs){
 										workers[w].balance = (workers[w].balance || 0);
 										workers[w].paid = (workers[w].paid || 0);
 										totalHash += portalStats.stats.pools[pool].workers[w].hashrate;
-										networkSols = portalStats.stats.pools[pool].poolStats.networkSols;
+										networkHash = portalStats.stats.pools[pool].poolStats.networkHash;
 									  }
 								  }
 								}
-								res.end(JSON.stringify({miner: address, totalHash: totalHash, totalShares: totalShares, networkSols: networkSols, immature: balances.totalImmature, balance: balances.totalHeld, paid: balances.totalPaid, workers: workers, history: history}));
+								res.end(JSON.stringify({miner: address, totalHash: totalHash, totalShares: totalShares, networkHash: networkHash, immature: balances.totalImmature, balance: balances.totalHeld, paid: balances.totalPaid, workers: workers, history: history}));
 							});
 						});
 					} else {

packages/portal/libs/paymentProcessor.js
@@ -386,7 +386,7 @@ function SetupForPool(logger, poolOptions, setupFinished) {
                     finalRedisCommands.push(['hset', coin + ':stats', 'networkDiff', result[0].response.difficulty]);
                 }
                 if (result[0].response.networkhashps !== null) {
-                    finalRedisCommands.push(['hset', coin + ':stats', 'networkSols', result[0].response.networkhashps]);
+                    finalRedisCommands.push(['hset', coin + ':stats', 'networkHash', result[0].response.networkhashps]);
                 }
 
                 daemon.cmd(poolOptions.coin.getInfo ? 'getinfo' : 'getnetworkinfo', params,

packages/portal/libs/stats.js
@@ -421,9 +421,8 @@ module.exports = function(logger, portalConfig, poolConfigs){
                                 invalidShares: replies[i + 2] ? (replies[i + 2].invalidShares || 0) : 0,
                                 totalPaid: replies[i + 2] ? (replies[i + 2].totalPaid || 0) : 0,
 								networkBlocks: replies[i + 2] ? (replies[i + 2].networkBlocks || 0) : 0,
-                                networkSols: replies[i + 2] ? (replies[i + 2].networkSols || 0) : 0,
-                                //networkSols: replies[i + 2] ? (replies[i + 2].networkBlocks || 0) : 0, 
-								networkSolsString: getReadableNetworkHashRateString(replies[i + 2] ? (replies[i + 2].networkSols || 0) : 0),
+                                networkHash: replies[i + 2] ? (replies[i + 2].networkHash || 0) : 0,
+								networkHashString: getReadableNetworkHashRateString(replies[i + 2] ? (replies[i + 2].networkHash || 0) : 0),
 								networkDiff: replies[i + 2] ? (replies[i + 2].networkDiff || 0) : 0,
 								networkConnections: replies[i + 2] ? (replies[i + 2].networkConnections || 0) : 0,
                                 networkVersion: replies[i + 2] ? (replies[i + 2].networkSubVersion || replies[i + 2].networkVersion) : 0,
@@ -584,7 +583,7 @@ module.exports = function(logger, portalConfig, poolConfigs){
                 coinStats.hashrateString = _this.getReadableHashRateString(coinStats.hashrate);
 
                 var _blocktime = coinStats.blockTime || 90;
-				var _networkHashRate = parseFloat(coinStats.poolStats.networkSols);
+				var _networkHashRate = parseFloat(coinStats.poolStats.networkHash);
 				coinStats.luckDays =  ((_networkHashRate / coinStats.hashrate * _blocktime) / (24 * 60 * 60)).toFixed(3);
                 coinStats.luckHours = ((_networkHashRate / coinStats.hashrate * _blocktime) / (60 * 60)).toFixed(3);
                 coinStats.luckMinute = ((_networkHashRate / coinStats.hashrate * _blocktime) / (60)).toFixed(3);
@@ -755,7 +754,6 @@ module.exports = function(logger, portalConfig, poolConfigs){
 		hashrate = (hashrate * 1000000);
 		if (hashrate < 1000000) {
 			return '0 H/s';
-			//return (Math.round(hashrate / 1000) / 1000 ).toFixed(2)+' Sol/s';
 		}
         var byteUnits = [' H/s', ' KH/s', ' MH/s', ' GH/s', ' TH/s', ' PH/s', ' EH/s', ' ZH/s', ' YH/s' ];
         var i = Math.floor((Math.log(hashrate/1000) / Math.log(1000)) - 1);

packages/portal/scripts/statsb.js
@@ -52,8 +52,7 @@ function calculateAverageBalance(pool) {
 function getReadableBalanceString(Balance){
 	Balance = (Balance * 1000000);
 	if (Balance < 1000000) {
-		return '0 Hash/s';
-		//return (Math.round(Balance / 1000) / 1000 ).toFixed(2)+' Sol/s';
+		return '0';
 	}
     var byteUnits = [ ' KOTO', ' kKOTO', ' MKOTO', ' GKOTO', ' TKOTO', ' PKOTO' ];
     var i = Math.floor((Math.log(Balance/1000) / Math.log(1000)) - 1);

packages/portal/website/pages/miner_stats.html
@@ -115,7 +115,7 @@
 <div id="boxesWorkers"> </div>
 
 <script>
-    var _miner = "{{=String(it.stats.address).split(".")[0]}}";
+    var _miner = "{{=String(it.stats.address).split('.')[0]}}";
     var _workerCount = 0;
     window.statsSource = new EventSource("/api/live_stats");
     document.querySelector('main').appendChild(document.createElement('script')).src = '/static/miner_stats.js';

packages/portal/website/pages/stats.html
@@ -112,13 +112,13 @@
 </div>
 {{ function capitalizeFirstLetter(t){return t.charAt(0).toUpperCase()+t.slice(1)} }}
 {{ function readableDate(a) {
-    var d = new Date(parseInt(a));
-    return d.getFullYear() + '/' +
-           (d.getMonth() + 1).toString().padStart(2, '0') + '/' +
-           d.getDate().toString().padStart(2, '0') + ' ' +
-           d.getHours().toString().padStart(2, '0') + ':' +
-           d.getMinutes().toString().padStart(2, '0') + ':' +
-           d.getSeconds().toString().padStart(2, '0');
+var d = new Date(parseInt(a));
+return d.getFullYear() + '/' +
+(d.getMonth() + 1).toString().padStart(2, '0') + '/' +
+d.getDate().toString().padStart(2, '0') + ' ' +
+d.getHours().toString().padStart(2, '0') + ':' +
+d.getMinutes().toString().padStart(2, '0') + ':' +
+d.getSeconds().toString().padStart(2, '0');
 } }}
 <div class="pure-g" id="boxesLower">
     {{ for(var pool in it.stats.pools) { }}
@@ -135,13 +135,12 @@
                             id="statsHashrate{{=pool}}">{{=it.stats.pools[pool].hashrateString}}</span> (Now)</div>
                     <div><i class="fas fa-gauge-simple-high fa-fw"></i> <span id="statsHashrateAvg{{=pool}}">...</span>
                         (Avg)</div>
-                    <div>
-                        <i class="fas fa-gavel fa-fw"></i> Luck: <span id="statsLuckDays{{=pool}}">
+                    <div><i class="fas fa-clock fa-fw"></i>&nbsp;Luck: <span id="statsLuckDays{=pool}}">
                             {{
                             function getReadableLuckTime(lucktime){
                             var luck = lucktime;
                             var timeUnits = [ ' Days', ' Hours', ' Minutes' ];
-                            if (luck < 1) { luck=luck * 24; if (luck < 1) { luck=luck * 60; return luck.toFixed(0) +
+                            if (luck < 1) { luck=luck * 24; if (luck < 1) { luck=luck * 60; return luck.toFixed(2) +
                                 timeUnits[2]; } else { return luck.toFixed(2) + timeUnits[1]; } } return luck +
                                 timeUnits[0]; } _luckTime=getReadableLuckTime(it.stats.pools[pool].luckDays); }}
                                 {{=_luckTime}}</span>
@@ -153,7 +152,7 @@
                             if(it.stats.pools[pool].hashrate == 0 ) {
                             percent=0.0;
                             } else {
-                            percent=it.stats.pools[pool].hashrate / it.stats.pools[pool].poolStats.networkSols * 100;
+                            percent=it.stats.pools[pool].hashrate / it.stats.pools[pool].poolStats.networkHash * 100;
                             }
                             }}
                             {{=percent.toFixed(5)}}
@@ -172,7 +171,7 @@
                             id="statsNetworkBlocks{{=pool}}">{{=it.stats.pools[pool].poolStats.networkBlocks}}</span>
                     </div>
                     <div><i class="fas fa-gauge-simple-high fa-fw"></i> <small>Network H/s:</small> <span
-                            id="statsNetworkSols{{=pool}}">{{=it.stats.pools[pool].poolStats.networkSolsString}}</span>
+                            id="statsnetworkHash{{=pool}}">{{=it.stats.pools[pool].poolStats.networkHashString}}</span>
                     </div>
                     <div><i class="fas fa-unlock fa-fw"></i> <small>Difficulty:</small> <span
                             id="statsNetworkDiff{{=pool}}">{{=(parseFloat(it.stats.pools[pool].poolStats.networkDiff)).toFixed(8)}}</span>
@@ -386,7 +385,7 @@
         .enter()
         .append('g')
         .attr('class', 'legend')
-        .attr('id', function() { return JSON.stringify(pool); })
+        .attr('id', function () { return JSON.stringify(pool); })
         .attr('transform', function (d, i) {
             var height = legendRectSize + legendSpacing;
             var offset = height * color.domain().length / 2;
@@ -428,9 +427,9 @@
                 $('#statsTotalPaid' + pool).text((parseFloat(stats.pools[pool].poolStats.totalPaid)).toFixed(8));
                 $('#statsNetworkBlocks' + pool).text(stats.pools[pool].poolStats.networkBlocks);
                 $('#statsNetworkDiff' + pool).text(stats.pools[pool].poolStats.networkDiff);
-                $('#statsNetworkSols' + pool).text(getReadableHashRateString(stats.pools[pool].poolStats.networkSols));
+                $('#statsnetworkHash' + pool).text(getReadableHashRateString(stats.pools[pool].poolStats.networkHash));
                 $('#statsNetworkConnections' + pool).text(stats.pools[pool].poolStats.networkConnections);
-                $('#statsHashPercent' + pool).text((stats.pools[pool].hashrate / stats.pools[pool].poolStats.networkSols * 100).toFixed(5));
+                $('#statsHashPercent' + pool).text((stats.pools[pool].hashrate / stats.pools[pool].poolStats.networkHash * 100).toFixed(5));
             }
         });
     });

packages/portal/website/static/stats.js
@@ -85,7 +85,6 @@ function getReadableHashRateString(hashrate){
         hashrate = (hashrate * 1000000);
         if (hashrate < 1000000) {
                 return '0 H/s';
-                //return (Math.round(hashrate / 1000) / 1000 ).toFixed(2)+' Sol/s';
         }
     var byteUnits = [' H/s', ' KH/s', ' MH/s', ' GH/s', ' TH/s', ' PH/s', ' EH/s', ' ZH/s', ' YH/s'];
     var i = Math.floor((Math.log(hashrate/1000) / Math.log(1000)) - 1);
@@ -94,23 +93,18 @@ function getReadableHashRateString(hashrate){
  }
 
  function getReadableLuckTime(lucktime){
-     var luck = lucktime;
-     var timeUnits = [' Days', ' Hours', ' Minutes', ' Seconds' ];
-     if (luck < 1) {
-         luck = luck * 24;
-         if (luck < 1) {
-             luck = luck * 60;
-             if (luck < 1) {
-                 luck = luck * 60;
-                     return luck.toFixed(2) + timeUnits[3];
-             } else {
-                     return luck.toFixed(2) + timeUnits[2];
-             }
-         } else {
-             return luck.toFixed(2) + timeUnits[1];
-         }
-     }
-     return luck + timeUnits[0];
+    var luck = lucktime;
+    var timeUnits = [ ' Days', ' Hours', ' Minutes' ];
+    if (luck < 1) {
+            luck = luck * 24;
+            if (luck < 1) {
+                    luck = luck * 60;
+                    return luck.toFixed(2) + timeUnits[2];
+            } else {
+                    return luck.toFixed(2) + timeUnits[1];
+            }
+    }
+    return luck.toFixed(3) + timeUnits[0];
 }
 
 function timeOfDayFormat(timestamp){

commit 1e06b8d67aa940ff2f9c80f3aafca8f195b04a97

fix: render worker page when a worker's luck is >= 1 day
getReadableLuckTime() received luckDays straight from /api/worker_stats,
where it is a string (the server formats it with .toFixed). For luck < 1
day the inner branches multiply luck (coercing it to a number) so .toFixed
worked, but the "Days" branch (luck >= 1) called .toFixed on the raw
string and threw "luck.toFixed is not a function". That throw happened in
rebuildWorkerDisplay() before updateStats(), so the page rendered nothing
-- no worker boxes and every stat stuck on "...".

Coerce the input with parseFloat() once at the top. Verified end-to-end
with a headless browser: the worker box and all stats now render.

Regression introduced in 8d8a401 (the Days branch went from string concat
to .toFixed).

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

packages/portal/website/static/miner_stats.js
@@ -31,7 +31,10 @@ function getReadableHashRateString(hashrate) {
 }
 
 function getReadableLuckTime(lucktime) {
-    let luck = lucktime;
+    // The API serves luck values as strings (server-side .toFixed). Coerce to a
+    // number so the "Days" branch (luck >= 1) doesn't call .toFixed on a string,
+    // which throws and aborts the whole render (no worker boxes, no stats).
+    let luck = parseFloat(lucktime);
     const timeUnits = [' Days', ' Hours', ' Minutes', ' Seconds'];
     if (luck < 1) {
         luck = luck * 24;